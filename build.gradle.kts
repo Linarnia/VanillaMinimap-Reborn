28232de83f040046e81b35b28f0045a0295e0ec9
Hieu Tran <hieu.tt2110@proton.me>
2025-07-27 20:35:34 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("io.papermc.paperweight.userdev").version("2.0.0-beta.8")
+    id("io.papermc.paperweight.userdev").version("2.0.0-beta.18")
     id("xyz.jpenilla.run-paper").version("2.3.1")
     id("com.gradleup.shadow").version("9.0.0-beta4")
 }
